db4bfedf9c9020ea4c1595e72b5aac8301b0a8be
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-20 12:37:33 +0530

    HashTableMain

---
+++ HashTable1/MyMapNode.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable1
+{
+    internal class MyMapNode<K, V>
+    {
+        public readonly int size;
+        private readonly LinkedList<KeyValuePair<K, V>>[] items;
+        public readonly V[] array;
+        public MyMapNode(int Size)
+        {
+            size = Size;
+            items = new LinkedList<KeyValuePair<K, V>>[Size];
+            array = new V[Size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public void Add(K key, V value)
+        {
+            int index = GetArrayPosition(key);
+            if (items[index]==null)
+            {
+                items[index] = new LinkedList<KeyValuePair<K, V>>();
+                items[index].AddLast(new KeyValuePair<K, V>(key, value));
+            }
+            else
+            {
+                Console.WriteLine($"Key ='{key}' and Value = '{value}' is not added in HashTable ");
+            }
+        }
+        public void Get(K key)
+        {
+            int index = GetArrayPosition(key);
+            bool Exist = false;
+            foreach (KeyValuePair<K, V> items in items[index])
+            {
+                if(items.Key.Equals(key))
+                {
+                    Exist = true;
+                    array[index] = items.Value;
+                }
+            }
+            if(!Exist)
+            {
+                Console.WriteLine($"key = '{key}' Doesn,t Exist in Hash Table");
+            }
+        }
+        public void FrequencyOfWords(V[] arr,int arrLength)
+        {
+            bool[] visited = new bool[arrLength];
+            int countWords = 0;
+            for(int i=0;i< arrLength;i++)
+            {
+                countWords++;
+                if (visited[i]==true)
+                {
+                    continue;
+                }
+                int count = 1;
+                for(int j=i+1;j<arrLength;j++)
+                {
+                    if (arr[i].Equals(arr[j]))
+                    {
+                        visited[j]=true;
+                        count++;
+                    }
+                }
+                Console.WriteLine($"'arr[i]' is Repeated {count} Times");
+            }
+            Console.Write($"Total Words are {countWords}");
+        }
+    }
+    public struct KeyValue<K,V>
+    {
+        public K Key { get; set; }  
+        public V Value { get; set; }
+    }
+}
---
+++ HashTable1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HashTable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<int, string> myMapNode = new MyMapNode<int ,string>(6);
+            myMapNode.Add(0,"To");
+            myMapNode.Add(1,"be");
+            myMapNode.Add(2,"or");
+            myMapNode.Add(3,"not");
+            myMapNode.Add(4,"to");
+            myMapNode.Add(5,"be");
+            myMapNode.Get(0);
+            myMapNode.Get(1);
+            myMapNode.Get(2);
+            myMapNode.Get(3);
+            myMapNode.Get(4);
+            myMapNode.Get(5);
+            Console.WriteLine(" The Added Statement is :: ");
+            for(int i=0;i< myMapNode.array.Length;i++)
+            {
+                Console.WriteLine($"{myMapNode.array[i]}");
+            }
+            Console.WriteLine(" Frequency of Words is : ");
+            myMapNode.FrequencyOfWords(myMapNode.array,myMapNode.array.Length);
+        }
+    }
+}
